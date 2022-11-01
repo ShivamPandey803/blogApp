5beec4a09b5a8ae8563f0a5a2694cd195565e3d2
Shivam Pandey <shivamp@WILCO.COM>
2022-11-01 15:02:15 +0530

    Changes in routes and Adding APi

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -5,11 +5,11 @@ export default function Header() {
     <div className="header">
       <div className="headerTitles">
         <span className="headerTitleSm">React & Node</span>
-        <span className="headerTitleLg">BLOG</span>
+        <span className="headerTitleLg">Blog</span>
       </div>
       <img
         className="headerImg"
-        src="https://img.freepik.com/free-photo/beautiful-scenery-phragmites-plants-by-sea-with-swimming-pelican-sunset_181624-37787.jpg"
+        src="https://images.pexels.com/photos/1167355/pexels-photo-1167355.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
         alt=""
       />
     </div>
